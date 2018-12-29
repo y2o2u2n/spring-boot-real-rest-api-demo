ec1a96c977d2e14a64aca8d273299c29d25eeace
Youn Junseok <y2o2u2n@gmail.com>
2018-12-29 22:50:53 +0900

    #1 init project

---
+++ src/main/java/me/y2o2u2n/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package me.y2o2u2n.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
