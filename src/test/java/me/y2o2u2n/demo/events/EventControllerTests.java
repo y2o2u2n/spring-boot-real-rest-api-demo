d103c4b0b5968fdcd65198265eca1a53e7507fd1
윤준석[지식iN][Apollo백엔드] <junseok.youn94@navercorp.com>
2018-12-31 14:13:30 +0900

    #4 fail on unknown properties

--- src/test/java/me/y2o2u2n/demo/events/EventControllerTests.java
+++ src/test/java/me/y2o2u2n/demo/events/EventControllerTests.java
@@ -32,6 +32,38 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2018, 12, 30, 23, 55))
+                .closeEnrollmentDateTime(LocalDateTime.of(2018, 12, 31, 23, 55))
+                .beginEventDateTime(LocalDateTime.of(2019, 1, 1, 23, 55))
+                .endEventDateTime(LocalDateTime.of(2019, 1, 2, 23, 55))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2 스타트업 팩토리")
+                .build();
+
+        mockMvc.perform(
+                post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event))
+
+        )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(Matchers.is(EventStatus.DRAFT.name())));
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -57,12 +89,7 @@ public class EventControllerTests {
 
         )
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(Matchers.is(EventStatus.DRAFT.name())));
+                .andExpect(status().isBadRequest())
+        ;
     }
 }
