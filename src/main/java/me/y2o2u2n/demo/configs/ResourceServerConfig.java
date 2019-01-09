8c9d307a55c7c2d41ce981b66948d567d758b6c4
y2o2u2n <y2o2u2n@gmail.com>
2019-01-09 17:19:32 +0900

    #10 fix wrong permit config

--- src/main/java/me/y2o2u2n/demo/configs/ResourceServerConfig.java
+++ src/main/java/me/y2o2u2n/demo/configs/ResourceServerConfig.java
@@ -23,7 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                 .and()
             .authorizeRequests()
                 .mvcMatchers(HttpMethod.GET, "/api/**")
-                    .anonymous()
+                    .permitAll()
                 .anyRequest()
                     .authenticated()
                 .and()
